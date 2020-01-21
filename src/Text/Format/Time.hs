3e78da9f1d1dea1c8b657ebddd79d4f5f6dbc6f6
Jorah Gao <jorah@version.cloud>
2020-01-21 14:52:53 +0800

    Extend vformat to FormatTime datatypes

--- src/Text/Format/Time.hs
+++ src/Text/Format/Time.hs
@@ -1,3 +1,66 @@
+{-| This module extend vformat to 'FormatTime' datatypes
+
+The specs of 'FormatTime' is same as the second argument of
+"Data.Time.Format.formatTime".
+
+=== Examples
+
+>>> utc <- getCurrentTime
+>>> zoned <- getZonedTime
+>>> let locale = utcToLocalTime (zonedTimeZone zoned) utc
+>>> format "{} / {} / {}" utc zoned locale
+"2020-01-21 05:54:41.137832 / 2020-01-21 13:54:45.310354 CST / 2020-01-21 13:54:41.137832"
+>>> format "{:%Y-%m-%dT%H:%M:%S}" utc
+"2020-01-21T05:54:41"
+-}
 module Text.Format.Time
-    (
+  ( formatTimeLocale
+  , formatTime
     ) where
+
+import           Data.Time.Calendar
+import           Data.Time.Clock
+import           Data.Time.Format    hiding (formatTime)
+import qualified Data.Time.Format    as T
+import           Data.Time.LocalTime
+import           Text.Format
+
+
+-- | Default specs is @%F@
+instance FormatArg Day where
+  formatArg = defaultSpecs "%F" formatTime
+
+-- | Default specs is @%F %T%Q@
+instance FormatArg UTCTime where
+  formatArg = formatTime
+
+-- | Default specs is @%F %T%Q@
+instance FormatArg UniversalTime where
+  formatArg = formatTime
+
+-- | Default specs is @%Z@
+instance FormatArg TimeZone where
+  formatArg = defaultSpecs "%Z" formatTime
+
+-- | Default specs is @%T@
+instance FormatArg TimeOfDay where
+  formatArg = defaultSpecs "%T" formatTime
+
+-- | Default specs is @%F %T%Q@
+instance FormatArg LocalTime where
+  formatArg = formatTime
+
+-- | Default specs is @%F %T%Q %Z@
+instance FormatArg ZonedTime where
+  formatArg = defaultSpecs "%F %T%Q %Z" formatTime
+
+
+{-| Formatter for 'FormatTime' values using a specific 'TimeLocale'. -}
+formatTimeLocale :: FormatTime t => TimeLocale -> t -> Formatter
+formatTimeLocale locale = defaultSpecs "%F %T%Q" $ \x k fmt ->
+  formatString (T.formatTime locale (fmtSpecs fmt) x) k (fmt{fmtSpecs=""})
+
+
+{-| Formatter for 'FormatTime' values using the 'defaultTimeLocale'. -}
+formatTime :: FormatTime t => t -> Formatter
+formatTime = formatTimeLocale defaultTimeLocale
--- test/Main.hs
+++ test/Main.hs
@@ -1,5 +1,49 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main ( main ) where
 
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+import           Control.Exception
 
+import           Data.Time.Calendar
+import           Data.Time.Clock
+import           Data.Time.Format
+import           Data.Time.LocalTime
+import           Text.Format
+import           Text.Format.Time    hiding (formatTime)
+
+main :: IO ()
+main = do
+  utc <- getCurrentTime
+  zoned <- getZonedTime
+  let tz = zonedTimeZone zoned
+      locale = utcToLocalTime tz utc
+      universal = read (show locale) :: UniversalTime
+      day = localDay locale
+      time = localTimeOfDay locale
+
+  putStrLn "\n<<<<<<<<<<<<<<<<<<<<<< Test <<<<<<<<<<<<<<<<<<<<<<"
+
+  it "UTCTime default specs" $
+    format "{}" utc == formatTimeDef "%F %T%Q" utc
+  it "ZonedTime default specs" $
+    format "{}" zoned == formatTimeDef "%F %T%Q %Z" zoned
+  it "LocalTime default specs" $
+    format "{}" locale == formatTimeDef "%F %T%Q" locale
+  it "UniversalTime default specs" $
+    format "{}" universal == formatTimeDef "%F %T%Q" universal
+  it "Day default specs" $
+    format "{}" day == formatTimeDef "%F" day
+  it "TimeZone default specs" $
+    format "{}" tz == formatTimeDef "%Z" tz
+  it "Customize specs" $
+    format "{:%Y-%m-%d}" utc == formatTimeDef "%Y-%m-%d" utc
+
+  putStrLn "\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+
+
+it :: String -> Bool -> IO ()
+it = flip assert . putStrLn
+
+
+formatTimeDef :: FormatTime t => String -> t -> String
+formatTimeDef = formatTime defaultTimeLocale
